9950f0af569b14aa38e5d9ba076fe9179747c6f7
yashtayde624 <yashtayde624@gmail.com>
2026-06-11 22:20:57 +0530

    Create day3.py

---
+++ day3.py
@@ -0,0 +1,8 @@
+Name = input("what is the name of the person?")
+match Name:
+ case "naruto"|"Kakashi"|"Hashirama":
+  print("they are from Leaf village")
+ case " ghara":
+  print(" sand village dude")
+ case "obito"|"madara"|"zoro":
+  print("they're lost Knights")
