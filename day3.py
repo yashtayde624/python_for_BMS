e2c919e8a6951c321915ea8b06f6e8749bd53c47
yashtayde624 <yashtayde624@gmail.com>
2026-07-02 11:49:53 +0530

    Delete day3.py

--- day3.py
+++
@@ -1,8 +0,0 @@
-Name = input("what is the name of the person?")
-match Name:
- case "naruto"|"Kakashi"|"Hashirama":
-  print("they are from Leaf village")
- case " ghara":
-  print(" sand village dude")
- case "obito"|"madara"|"zoro":
-  print("they're lost Knights")
