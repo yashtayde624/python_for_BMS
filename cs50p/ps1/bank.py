0961d5c7629032bafb22775b8f03a620f72d904b
yashtayde624 <yashtayde624@gmail.com>
2026-07-21 18:15:40 +0530

    Update bank.py

--- cs50p/ps1/bank.py
+++ cs50p/ps1/bank.py
@@ -1,1 +1,13 @@
 
+def main():
+    x = input("greet")
+    x = x.strip().lower()
+    if x == "hello" or x.startswith("hello"):
+        print("$0")
+    elif x.startswith("h"):
+        print("$20")
+    else:
+        print("$100")
+
+
+main()
